52647a224fa17816b3a8055abdf5d5b237113b48
domespa <dumiii1988@gmail.com>
2025-12-16 10:15:07 +0100

    fix scrolltotop

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
-import ScrollToTop from "./components/ScrolltoTop.jsx";
+import ScrollToTop from "./components/ScrollToTop.jsx";
 import "./index.css";
 import App from "./App.jsx";
 
